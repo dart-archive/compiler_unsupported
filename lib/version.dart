55f0d9838a5f5666612486a3905572061709cf51
Devon Carew <devoncarew@gmail.com>
2015-09-14 22:32:26 -0700

    1.12.1

--- lib/version.dart
+++ lib/version.dart
@@ -2,5 +2,5 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-final String version = '1.12.0';
-final String versionLong = '1.12.0';
+final String version = '1.12.1';
+final String versionLong = '1.12.1';
