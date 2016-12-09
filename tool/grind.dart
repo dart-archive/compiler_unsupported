a6304732526e1ba67510a9b1f9accb0de499271d
Luke Church <lukechurch@google.com>
2016-12-09 10:14:41 +0000

    Update version tag

--- tool/grind.dart
+++ tool/grind.dart
@@ -9,7 +9,7 @@ import 'dart:io';
 final Directory trunk = new Directory('trunk');
 
 // The sdk repo version to download.
-const String sdkTag = '1.20.0';
+const String sdkTag = '1.21.0';
 
 main(List<String> args) => grind(args);
 
